dbe64ced5bbeb692214b009d9a8bb99f91afd735
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2022-11-21 21:04:08 +0200

    initial commit create App project file

---
+++ PexelsVideoPlayer/PexelsVideoPlayer/PexelsVideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PexelsVideoPlayerApp.swift
+//  PexelsVideoPlayer
+//
+//  Created by Ahmed Amin on 21/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct PexelsVideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
