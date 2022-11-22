36b366d327b1446a73dc2f918ea7f69782d9a1aa
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2022-11-22 15:19:38 +0200

    implement video player view with AVKit framwork

---
+++ PexelsVideoPlayer/PexelsVideoPlayer/View/VideoView.swift
@@ -0,0 +1,32 @@
+//
+//  VideoView.swift
+//  PexelsVideoPlayer
+//
+//  Created by Ahmed Amin on 22/11/2022.
+//
+
+import SwiftUI
+import AVKit
+
+struct VideoView: View {
+    var video: Video
+    @State private var player: AVPlayer = AVPlayer()
+    
+    var body: some View {
+        VideoPlayer(player: player)
+            .edgesIgnoringSafeArea(.all)
+            .onAppear {
+                guard let link = video.videoFiles?.first?.link,
+                let url = URL(string: link) else { return }
+                
+                player = AVPlayer(url: url)
+                player.play()
+            }
+    }
+}
+
+struct VideoView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoView(video: previewVideo)
+    }
+}
